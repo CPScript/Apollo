18fbcdd7bb123a1ecb348e3ae3c0d339b5dc375a
Chai <83523587+CPScript@users.noreply.github.com>
2023-04-19 09:18:42 -0400

    Add files via upload

---
+++ 3/install.sh
@@ -0,0 +1,18 @@
+apt-get install wget
+
+pip install wget
+
+py -m pip install wget
+
+python -m pip install wget
+
+apt-get install python3
+
+apt-get install git
+
+python3 Server.py
+
+clear
+
+python3 Saturn.py
+
